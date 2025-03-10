68e526ed554387e5ad517f15e5ecd2fe81cf364b
anvil shakya <anvilsky123tg@gamil.com>
2025-03-10 13:23:00 +0545

    added time to cart and sales to calcualate the actual sales by simultaenously adding the sales

--- books.h
+++ books.h
@@ -1,13 +1,12 @@
 
 #ifndef BOOKS_H
 #define BOOKS_H
-
+#include<time.h>
 struct books {
     char book[100];
     char author[100];
     char genre[100];
     int price;
-    
+    char date_of_purchase[20];
 };
-
 #endif 
